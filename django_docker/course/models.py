070460bc32a53d554065abf1f498e4f1610238d4
rasbin <rasbin.thapa@learnsic.com>
2025-04-21 16:27:58 +0545

    minor updates

--- django_docker/course/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Course(models.Model):
-    title = models.CharField(max_length=200, blank=True, null=True)
-    author = models.CharField(max_length=100, blank=True, null=True)
-
-    def __str__(self):
-        return self.title
--- django_graphql_auth/core/urls.py
+++ django_graphql_auth/core/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 from .schema import schema
@@ -23,5 +24,5 @@ from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("graphql/", GraphQLView.as_view(graphiql=True, schema=schema)),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
