cc7e684cc98ec288131d11be0917d68a87556b4b
rasbin <rasbingodarthapa@gmail.com>
2025-05-05 17:10:03 +0545

    updated test

--- mixed/blog/tests.py
+++ mixed/blog/tests.py
@@ -1,11 +1,54 @@
 import json 
 from django.contrib.auth import get_user_model
+from django.test import TestCase
 from graphene_django.utils.testing import GraphQLTestCase
 
 from .models import Post
 
 User = get_user_model()
 
+
+class TestPostModel(TestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create(
+            name="test1", email="test1@test.com", password="admin"
+        )
+        self.p1 = Post.objects.create(
+            title = "test 1",
+            content = "test 1 content",
+            published= True,
+            owner=self.user1
+        )
+
+        self.p2 = Post.objects.create(
+            title = "test 2",
+            content = "test 2 content",
+            published= True,
+            owner=self.user1
+        )
+
+        self.p3 = Post.objects.create(
+            title = "test 3",
+            content = "test 3 content",
+            published= True,
+            owner=self.user1
+        )
+
+    def test_all_post_retreival(self):
+        posts = Post.objects.all()
+        self.assertEqual(len(posts), 3)
+    
+    def test_create_post(self):
+        self.p4 = Post.objects.create(
+            title = "test 4",
+            content = "test 4 content",
+            published= True,
+            owner=self.user1
+        )
+        self.assertEqual(self.p4.title, "test 4")
+
+        
 class TestPostView(GraphQLTestCase):
     GRAPHQL_URL = "/graphql/"
 
