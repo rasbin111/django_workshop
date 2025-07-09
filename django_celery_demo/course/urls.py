773ab7383d74050039a44f12ee73e8ce6bacf7ba
rasbin <rasbingodarthapa@gmail.com>
2025-07-09 16:55:33 +0545

    minor changes

--- django_celery_demo/course/urls.py
+++ django_celery_demo/course/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import course_home, check_task_status, run_periodic_tasks
 urlpatterns = [
-        path("task/", course_home),
-        path("task/<str:task_id>/", check_task_status),
-        path("periodic_task/", run_periodic_tasks),
+    path("task/", course_home),
+    path("task/<str:task_id>/", check_task_status),
+    path("periodic_task/", run_periodic_tasks),
 ]
