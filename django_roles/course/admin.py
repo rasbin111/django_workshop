d9c363f27f29d1f0536b6e12098fe7e6f10c2e57
rasbin <rasbingodarthapa@gmail.com>
2025-08-22 12:29:39 +0545

    minor updates

--- django_roles/course/admin.py
+++ django_roles/course/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Course, Lesson
+
+admin.site.register([Category, Course, Lesson])
--- django_roles/useraccount/models.py
+++ django_roles/useraccount/models.py
@@ -32,3 +32,4 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
 
+
